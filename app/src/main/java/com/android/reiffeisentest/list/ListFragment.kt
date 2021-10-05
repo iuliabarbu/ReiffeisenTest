4f4fa865f7464276a53eab19d673d2c6d27e3639
iuliabarbu <iulia_marin@hotmail.com>
2021-10-06 00:14:19 +0300

    - add reference to ListViewModel in the ListFragment.kt

--- app/src/main/java/com/android/reiffeisentest/list/ListFragment.kt
+++ app/src/main/java/com/android/reiffeisentest/list/ListFragment.kt
@@ -1,6 +1,32 @@
 package com.android.reiffeisentest.list
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.android.reiffeisentest.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
+    val TAG = ListFragment::class.java.simpleName as String
+
+    private val viewModel: ListViewModel by lazy {
+        ViewModelProvider(this).get(ListViewModel::class.java)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentListBinding.inflate(inflater)
+        binding.lifecycleOwner = this.viewLifecycleOwner
+        binding.viewModel = viewModel
+
+        binding.itemsRecycler.adapter = ResultsAdapter()
+
+        return binding.root
+    }
+
+
 }
